705eb3a69f9f30729be7bd756a773ed45ca8d56f
Hu Chao Xiang <charles@HumatoMacBook-Pro.local>
2018-11-03 02:36:18 -0400

    Updates

---
+++ precache-manifest.9e877793b30fdb0d9b964886b8fbdbbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8abe40b2c1fc2b02ef2b7def51b43f7e",
+    "url": "/react-gh-pages/static/media/bg-5-full.8abe40b2.jpg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "4809dd14411042207960",
+    "url": "/react-gh-pages/static/js/main.4809dd14.chunk.js"
+  },
+  {
+    "revision": "378b5440c897e0ae8469",
+    "url": "/react-gh-pages/static/js/1.378b5440.chunk.js"
+  },
+  {
+    "revision": "4809dd14411042207960",
+    "url": "/react-gh-pages/static/css/main.31855bff.chunk.css"
+  },
+  {
+    "revision": "eb1d35865a022afac3e2b0640fffdc70",
+    "url": "/react-gh-pages/index.html"
+  }
+];
